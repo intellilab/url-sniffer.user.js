1cbaa90111c49480b2315af55b12cdef925d0295
gera2ld <gera2ld@live.com>
2021-02-24 17:34:35 +0000

    Deploying to release from master @ 4264ae4a478da5ad7a3ad3dae206f91200574fe0 🚀

--- index.user.js
+++ index.user.js
@@ -3,6 +3,7 @@
 // @name:zh-CN  URL 嗅探器
 // @namespace   https://gera2ld.space/
 // @description Sniff URLs in HTML
+// @description:zh-CN  从 HTML 中嗅探 URL
 // @match       *://*/*
 // @grant       GM_addStyle
 // @grant       GM_registerMenuCommand
