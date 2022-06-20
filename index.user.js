30ceacd160d170d88deeaf4d8da5343724f85a61
gera2ld <gera2ld@live.com>
2022-06-20 21:47:44 +0000

    Deploying to release from master @ 3d699c674604ef17f4f8447bdf2b0e48ede4cf62 🚀

--- index.user.js
+++ index.user.js
@@ -1,25 +1,60 @@
+
 // ==UserScript==
-// @name        URL Sniffer
-// @name:zh-CN  URL 嗅探器
-// @namespace   https://gera2ld.space/
-// @description Sniff URLs in HTML
-// @description:zh-CN  从 HTML 中嗅探 URL
-// @match       *://*/*
-// @grant       GM_addStyle
-// @grant       GM_registerMenuCommand
-// @grant       GM_unregisterMenuCommand
-// @grant       GM_setClipboard
-// @version     0.1.0
-// @author      Gerald <gera2ld@live.com>
-// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@1,npm/@violentmonkey/ui@0.5
+// @name              URL Sniffer
+// @name:zh-CN        URL 嗅探器
+// @namespace         https://gera2ld.space/
+// @description       Sniff URLs in HTML
+// @description:zh-CN 从 HTML 中嗅探 URL
+// @match             *://*/*
+// @version           0.2.0
+// @author            Gerald <gera2ld@live.com>
+// @require           https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
+// @supportURL        https://github.com/intellilab/url-sniffer.user.js
+// @grant             GM_addStyle
+// @grant             GM_registerMenuCommand
+// @grant             GM_setClipboard
+// @grant             GM_unregisterMenuCommand
 // ==/UserScript==
 
 (function () {
 'use strict';
 
-var styles = {"root":"style-module_root__3T7HH","toast":"style-module_toast__3PP-f"};
-var stylesheet=".style-module_root__3T7HH{position:fixed;top:0;right:0;bottom:0;left:0;z-index:10000;background:rgba(0,0,0,.53333)}.style-module_root__3T7HH>*{position:absolute;top:0;left:0;border:2px solid}.style-module_toast__3PP-f{z-index:10001!important}";
+function _extends() {
+  _extends = Object.assign ? Object.assign.bind() : function (target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
 
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  };
+  return _extends.apply(this, arguments);
+}
+
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+var styles = {"root":"style-module_root__1vyw2","toast":"style-module_toast__OcS5G","image":"style-module_image__1P0bD"};
+var stylesheet=".style-module_root__1vyw2{background:#0008;inset:0;position:fixed;z-index:10000}.style-module_root__1vyw2:before{background:#0008;color:#bbb;content:\"Double click anywhere on the mask to exit\";font-size:12px;left:50%;padding:8px 16px;position:absolute;top:0;transform:translateX(-50%)}.style-module_root__1vyw2>*{border:2px solid;left:0;position:absolute;top:0}.style-module_toast__OcS5G{z-index:10001!important}.style-module_image__1P0bD{background:#0008;inset:80px;overflow:auto;position:absolute}.style-module_image__1P0bD>img{position:absolute;transform-origin:top left}";
+
+const _excluded = ["elements", "getItem"];
 const STYLE_CURRENT = {
   stroke: '#0f08',
   fill: '#0f02'
@@ -40,6 +75,9 @@ const STYLE_TO_SELECT = {
   stroke: '#bb08',
   fill: '#bb02'
 };
+const MODE_SINGLE = 0;
+const MODE_MULTIPLE = 1;
+let rendering = false;
 const mask = VM.getHostElement(false);
 mask.addStyle(stylesheet);
 mask.root.className = styles.root;
@@ -47,35 +85,131 @@ mask.root.addEventListener('mousedown', handleMouseDown);
 mask.root.addEventListener('mouseup', handleMouseUp);
 mask.root.addEventListener('mousemove', handleMouseMove);
 mask.root.addEventListener('click', handleClick);
-GM_registerMenuCommand('Toggle sniffer', toggle);
+mask.root.addEventListener('dblclick', handleCallback);
+GM_registerMenuCommand('Sniff links', sniffLinks);
+GM_registerMenuCommand('Sniff images', sniffImages);
 let context;
 
-function toggle() {
-  if (context) close();else start();
+function sniffLinks() {
+  if (context) close();
+  start({
+    elements: document.querySelectorAll('a[href]'),
+
+    getItem(el) {
+      const href = el.tagName.toLowerCase() === 'a' && el.getAttribute('href');
+      if (href && !/^(?:#|javascript:)/.test(href)) return {
+        el
+      };
+    },
+
+    mode: MODE_MULTIPLE,
+
+    callback(selectedItems) {
+      copy(selectedItems);
+      close();
+    }
+
+  });
 }
 
-function start() {
-  if (context) return;
-  const items = Array.from(document.querySelectorAll('a[href]')).filter(a => {
-    const href = a.getAttribute('href');
-    return href && !/^(?:#|javascript:)/.test(href);
-  }).map(el => ({
-    el
-  }));
-  context = {
+function sniffImages() {
+  if (context) close();
+  const imageViewer = VM.hm("div", {
+    className: styles.image,
+    onClick: e => e.stopPropagation()
+  });
+
+  const showImage = img => {
+    mask.root.append(imageViewer);
+    const {
+      naturalWidth,
+      naturalHeight
+    } = img;
+    const containerWidth = imageViewer.clientWidth;
+    const containerHeight = imageViewer.clientHeight;
+    const scale = Math.min(1, containerWidth / naturalWidth);
+    const width = naturalWidth * scale;
+    const height = naturalHeight * scale;
+    const x = Math.max(0, (containerWidth - width) / 2);
+    const y = Math.max(0, (containerHeight - height) / 2);
+    imageViewer.innerHTML = '';
+    imageViewer.append(img);
+    img.style.transform = `scale(${scale}) translate(${x}px,${y}px)`;
+    context.paused = true;
+    mask.root.addEventListener('click', closeViewer);
+  };
+
+  const closeViewer = () => {
+    imageViewer.innerHTML = '';
+    imageViewer.remove();
+    context.paused = false;
+    mask.root.removeEventListener('click', closeViewer);
+  };
+
+  start({
+    getItem(el) {
+      let url;
+
+      if (el.tagName.toLowerCase() === 'img') {
+        url = el.src;
+      } else {
+        const bgImg = el.style.backgroundImage.match(/^url\((['"]?)(.*?)\1\)/);
+        url = bgImg == null ? void 0 : bgImg[2];
+      }
+
+      return url && {
+        el,
+        url
+      };
+    },
+
+    mode: MODE_SINGLE,
+
+    callback([item]) {
+      if (!item) return close();
+      const img = new Image();
+      img.src = item.url;
+
+      img.onload = () => {
+        showImage(img);
+      };
+    }
+
+  });
+}
+
+function start(opts) {
+  if (context) throw new Error('Context already exists');
+
+  const {
+    elements,
+    getItem
+  } = opts,
+        rest = _objectWithoutPropertiesLoose(opts, _excluded);
+
+  const items = Array.from(elements || document.querySelectorAll('*')).map(getItem).filter(Boolean);
+  context = _extends({}, rest, {
     items,
     index: -1,
-    active: null
-  };
+    active: null,
+    disconnect: VM.observe(document.body, mutations => {
+      mutations.forEach(mut => {
+        if (mut.type === 'childList') {
+          const newItems = Array.from(mut.addedNodes).filter(el => !mask.root.contains(el)).map(getItem).filter(Boolean);
+          context.items.push(...newItems);
+        }
+      });
+    })
+  });
   update();
   mask.show();
   document.addEventListener('scroll', update);
   document.addEventListener('resize', update);
-  GM_registerMenuCommand('Copy URLs', handleCopy);
 }
 
 function close() {
   if (!context) return;
+  context.disconnect == null ? void 0 : context.disconnect();
   mask.root.innerHTML = '';
   mask.hide();
   context = null;
@@ -85,16 +219,21 @@ function close() {
 }
 
 function update() {
-  context.items.forEach(item => {
-    const rect = item.el.getBoundingClientRect();
-    item.pos = {
-      x: rect.left,
-      y: rect.top,
-      w: rect.width,
-      h: rect.height
-    };
+  if (rendering) return;
+  rendering = true;
+  requestAnimationFrame(() => {
+    context.items.forEach(item => {
+      const rect = item.el.getBoundingClientRect();
+      item.pos = {
+        x: rect.left,
+        y: rect.top,
+        w: rect.width,
+        h: rect.height
+      };
+    });
+    render();
+    rendering = false;
   });
-  render();
 }
 
 function render() {
@@ -125,7 +264,7 @@ function renderActive() {
     }
   } else {
     if (!context.active) {
-      context.active = VM.createElement(mask.id, null);
+      context.active = VM.hm(mask.id, null);
       updateStyle(context.active, STYLE_CURRENT);
       mask.root.append(context.active);
     }
@@ -143,7 +282,7 @@ function renderSelected() {
 function setItemRect(item, style) {
   if (style) {
     if (!item.rect) {
-      item.rect = VM.createElement(mask.id, null);
+      item.rect = VM.hm(mask.id, null);
       mask.root.append(item.rect);
     }
 
@@ -156,16 +295,21 @@ function setItemRect(item, style) {
 }
 
 function handleClick() {
+  if (context.paused) return;
   const activeItem = context.items[context.index];
 
   if (activeItem) {
-    activeItem.selected = !activeItem.selected;
-    setItemRect(activeItem, activeItem.selected && STYLE_SELECTED);
+    if (context.mode === MODE_SINGLE) {
+      context.callback([activeItem]);
+    } else {
+      activeItem.selected = !activeItem.selected;
+      setItemRect(activeItem, activeItem.selected && STYLE_SELECTED);
+    }
   }
 }
 
 function handleMouseDown(e) {
-  if (context.dragging) return;
+  if (context.dragging || context.mode === MODE_SINGLE || context.paused) return;
   const x = e.clientX;
   const y = e.clientY;
   context.dragging = {
@@ -175,12 +319,13 @@ function handleMouseDown(e) {
 }
 
 function handleMouseMove(e) {
+  if (context.paused) return;
   const x = e.clientX;
   const y = e.clientY;
 
   if (context.dragging) {
     if (!context.dragging.rect) {
-      const rect = VM.createElement(mask.id, null);
+      const rect = VM.hm(mask.id, null);
       updateStyle(rect, STYLE_SELECTION);
       mask.root.append(rect);
       context.dragging.rect = rect;
@@ -234,13 +379,19 @@ function handleMouseUp() {
   context.dragging = null;
 }
 
-function handleCopy() {
-  const urls = context.items.filter(item => item.selected).map(item => item.el.href);
-  GM_setClipboard(urls.join('\n'));
+function handleCallback() {
+  const selectedItems = context.items.filter(item => item.selected);
+  context.callback(selectedItems);
+}
+
+function copy(selectedItems) {
+  const urls = selectedItems.map(item => item.el.href);
+  if (!urls.length) return;
+  GM_setClipboard(urls.join('\r\n'));
   VM.showToast('URLs copied', {
     shadow: false,
     className: styles.toast
   });
 }
 
-}());
+})();
